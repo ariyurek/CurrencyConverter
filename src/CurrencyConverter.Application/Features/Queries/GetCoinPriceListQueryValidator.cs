4d82c29eda1db5ad74993004909fdf2bf8d1bc78
Alper Ariyurek <ARIY372606@exactsoftware.com>
2021-05-21 11:34:19 +0200

    query validator is added

---
+++ src/CurrencyConverter.Application/Features/Queries/GetCoinPriceListQueryValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace CurrencyConverter.Application.Features.Queries
+{
+    public class GetCoinPriceListQueryValidator : AbstractValidator<GetCoinPriceListQuery>
+    {
+        public GetCoinPriceListQueryValidator()
+        {
+            RuleFor(p => p.CoinCode)
+                .NotEmpty().WithMessage("{CoinCode} is required.")
+                .NotNull()
+                .MinimumLength(3).WithMessage("{CoinCode} minimum length should be 3.");
+        }
+    }
+}
